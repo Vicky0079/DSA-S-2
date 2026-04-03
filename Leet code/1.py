4073183f1eb0ce8529a3c01ded032be31b04b414
Vicky0079 <vickylucky00079@gmail.com>
2026-04-03 23:03:20 +0530

    1

---
+++ Leet code/1.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        hash_map = {}
+
+        for i in range(n):
+            remaining = target - nums[i]
+            if remaining in hash_map:
+                return [hash_map[remaining], i]
+            hash_map[nums[i]] = i  
+obj = Solution()
+# result = obj.twoSum([5,1,3,7],8)
+# print(result)
+print(obj.twoSum([5,1,3,7],8))
